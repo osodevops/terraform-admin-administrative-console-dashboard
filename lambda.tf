7425912d3c18ca73244b68169ba38176ff717153
jjcallis <jjcallis@users.noreply.github.com>
2022-07-29 16:35:31 +0100

    lambda

---
+++ lambda.tf
@@ -0,0 +1,23 @@
+resource "aws_lambda_function" "adminconsoledataprepare" {
+  filename      = "data_prepare.zip"
+  function_name = "data_prepare"
+  description = "admin console dataprepare lambda function"
+
+  environment {
+    variables = {
+        aws_region = data.aws_region.current.name
+    }
+  }
+  handler = "data_prepare.lambda_handler"
+  source_code_hash = filebase64sha256("data_prepare.zip")
+
+  runtime = "python3.7"
+  memory_size = "512"
+  timeout = "900"
+  role = aws_iam_role.quick_sight_admin_console.arn
+
+#   code_signing_config_arn = {
+#     S3Bucket = "admin-console-dataprepare-code"
+#     S3Key = "data_prepare.zip"
+#   }
+}
