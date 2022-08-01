c9a0df828ad827b1e3131c02662f5295b805502a
jjcallis <jjcallis@users.noreply.github.com>
2022-08-01 14:13:54 +0100

    change bucket to use the aws module

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,11 +1,16 @@
-resource "aws_s3_bucket" "adminconsole" {
+module "adminconsole_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
   bucket = join("", ["admin-console", data.aws_caller_identity.current.account_id])
-
-#   tags = var.common_tags
-}
-
-
-resource "aws_s3_bucket_acl" "adminconsole" {
-  bucket = aws_s3_bucket.adminconsole.id
   acl    = "private"
+
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  attach_deny_insecure_transport_policy = true
+  attach_require_latest_tls_policy      = true
+
 }
